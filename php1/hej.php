e21c8d8c4b88d7f3e1c5957cd72b37ca2eadf300
elvland <olleelvland@gmail.com>
2020-11-12 21:02:06 +0100

    form

--- php1/hej.php
+++ php1/hej.php
@@ -1,6 +1,14 @@
-<?php 
+<?php
+$servername   = "127.0.0.1";
+$database = "db970801";
+$username = "970801";
+$password = "monkagiga123";
 
-
-echo "Början på något HELT INSANE!!!!"
-
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed cuse du suger: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
